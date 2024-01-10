1e6939099429f0e0c5d5cacd800ff6b521fab6bd
hujuu <1961946+hujuu@users.noreply.github.com>
2024-01-10 09:56:13 +0900

    Create first.sol

---
+++ contracts/first.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+
+contract first {
+    int public mydata;
+
+    function GetDouble(int _data) public returns (int
+        _output) {
+        mydata = _data * 2;
+        return mydata;
+    }
+}
