21a859f88b37552707454e16b7225944e7de219a
hujuu <1961946+hujuu@users.noreply.github.com>
2024-01-10 12:35:05 +0900

    Update TestFirst.sol

--- test/TestFirst.sol
+++ test/TestFirst.sol
@@ -1,8 +1,14 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.0;
 
-contract TestFirst {
-    constructor(){
+import "truffle/Assert.sol";
+import "truffle/DeployedAddresses.sol";
+import "../contracts/first.sol";
 
+contract TestFirst {
+    function testInitialBalanceUsingDeployedContract() public {
+        first meta = first(DeployedAddresses.first());
+
+        Assert.equal(meta.GetDouble(10), 20, "done");
     }
 }
