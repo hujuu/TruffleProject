787950de9f25aee7d50f3d7fbf524267586b37fa
hujuu <1961946+hujuu@users.noreply.github.com>
2024-01-10 12:08:54 +0900

    Update first.sol

--- contracts/first.sol
+++ contracts/first.sol
@@ -4,8 +4,7 @@ pragma solidity ^0.8.0;
 contract first {
     int public mydata;
 
-    function GetDouble(int _data) public returns (int
-        _output) {
+    function GetDouble(int _data) public returns (int _output) {
         mydata = _data * 2;
         return mydata;
     }
